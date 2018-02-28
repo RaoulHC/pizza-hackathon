ea09e620b99c9e2176136d8853512b347ef0bebc
JacobRawling <mrjacob3@gmail.com>
2018-02-28 16:14:04 +0000

    First dummy reading in file done.

---
+++ pizza.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# LOAD THE INPUT 
+with open('example.in','r') as f:
+    lines = f.readlines()
+
+# First line is R C L H 
+nums = [int(s) for s in lines[0].split(' ')]
+R,C,L,H = nums[0],nums[1],nums[2],nums[3]
+print "R = ", R
+print "C = ", C
+print "L = ", L
+print "H = ", H
+
+# Now get the pizza as a matrix
+pizza = []
+for line in lines[1:]:
+	line = line.replace('T','0 ')
+	line = line.replace('M','1 ')
+	line = line.replace('\n','')
+	line = line[:-1]
+	pizza.append([ int(s) for s in line.split(' ')])
+
+# convert to a numpy array 
+pizza = np.array(pizza)
+print "PIZZA: "
+print pizza
+
+
+# PROCESS THE PIZZA
+
+# SAVE THE RESULT
